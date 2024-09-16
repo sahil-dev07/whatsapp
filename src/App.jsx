fcc16cc3ba44aacd36f02b5d1382ed04e09c700c
Sahil <sahilgupta86046@gmail.com>
2024-09-16 11:41:43 +0530

    logout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 // import React, { useState } from 'react';
-import SideBar from './component/SideBar';
-import Chat from './component/Chat';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
-import { useStateValue } from './StateProvider'
+import SideBar from "./component/SideBar"
+import Chat from "./component/Chat"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import { useStateValue } from "./StateProvider"
 
-import './app.css'
-import Login from './component/Login';
+import "./app.css"
+import Login from "./component/Login"
+import { useEffect } from "react"
+import { auth } from "./firebase"
+import { onAuthStateChanged } from "firebase/auth"
+import { actionTypes } from "./reducer"
 
 function App() {
-  const [{ user }] = useStateValue()
+  const [{ user }, dispatch] = useStateValue()
   // const [messages, setmessages] = useState([])
   // const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("useeffect user : ", user)
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: user,
+        })
+      } else {
+        // setUser(null)
+      }
+    })
+    return () => unsubscribe()
+  }, [auth])
 
   return (
     <div className="app">
@@ -18,22 +37,27 @@ function App() {
         <Login />
       ) : (
         <div className="app__body">
-          <BrowserRouter >
+          <BrowserRouter>
             <SideBar />
             <Routes>
-              <Route path='/' element={<Chat />} />
+              <Route path="/" element={<Chat />} />
 
-              <Route path='/rooms/:roomId' element={<Chat />} />
-              <Route path='/home' element={<Link to='/'> <div>click me</div> </Link>} />
-
+              <Route path="/rooms/:roomId" element={<Chat />} />
+              <Route
+                path="/home"
+                element={
+                  <Link to="/">
+                    {" "}
+                    <div>click me</div>{" "}
+                  </Link>
+                }
+              />
             </Routes>
           </BrowserRouter>
         </div>
-
       )}
-
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
